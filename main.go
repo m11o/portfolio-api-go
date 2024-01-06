ce7a3355187eaa801f198d12028a83de9a8a2495
Masahito Osako <masahito.osako@hey.com>
2024-01-06 23:04:06 +0900

    create documents and read documents from firestore emulator

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/api/iterator"
+	"log"
+
+	"cloud.google.com/go/firestore"
+)
+
+func createClient(ctx context.Context) *firestore.Client {
+	// Sets your Google Cloud Platform project ID.
+	projectID := "dummy-project"
+
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	// Close client when done with
+	// defer client.Close()
+	return client
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+	ctx := context.Background()
+	client := createClient(ctx)
+	_, _, err := client.Collection("users").Add(ctx, map[string]interface{}{
+		"first": "Ada",
+		"last":  "Lovelace",
+		"born":  1815,
+	})
+	if err != nil {
+		log.Fatalf("Failed adding alovelace: %v", err)
+	}
+
+	iter := client.Collection("users").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		fmt.Println(doc.Data())
+	}
+}
